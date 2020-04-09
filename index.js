46f0952f23615fdd81840cafed0e9e48c964e481
sojib-bd <sojibrahmatuzzaman@gmail.com>
2020-04-09 15:33:08 +0600

    added the nodemongo file to the repository

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const uri = process.env.DB_PATH;
+
+let client = new MongoClient(uri, { useNewUrlParser: true });
+
+
+app.get('/viewAppointment', (req, res) => {
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("doctorsPortal").collection("appointments");
+        collection.find().toArray((err, documents) => {
+            if (err) {
+                console.log(err)
+                res.status(500).send({ message: err });
+            }
+            else {
+                res.send(documents);
+            }
+        });
+        client.close();
+    });
+});
+
+
+app.post('/appointment', (req, res) => {
+    const appointment = req.body;
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("doctorsPortal").collection("appointments");
+        collection.insert(appointment, (err, result) => {
+            if (err) {
+                console.log(err)
+                res.status(500).send({ message: err });
+            }
+            else {
+                res.send(result.ops[0]);
+            }
+        });
+        client.close();
+    });
+});
+
+const port = process.env.PORT || 4200;
+app.listen(port, () => console.log(`Listenting to port ${port}`)); 
